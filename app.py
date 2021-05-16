ec146fc5ab7cef46222a04695e97f1cf7d714b31
alex-toy <alexei.80@hotmail.fr>
2021-05-16 11:45:41 +0200

    init

--- app.py
+++ app.py
@@ -6,4 +6,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-f
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
