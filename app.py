17d8ae14f6666558f6e1ed90b90b7fa426497363
alex-toy <alexei.80@hotmail.fr>
2021-05-16 11:52:37 +0200

    progress data

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import sqlite3
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
+
+def test():
+    query = f"""
+        SELECT * FROM technos
+        WHERE type = '{application_type}';
+    """
+    conn = sqlite3.connect('developpers.db')
+    df = pd.read_sql(query, conn)
+
+    for index, row in df.iterrows():
+        content = f"{row['name']}"
+        dispatcher.utter_message(text=content)
 
 
 
