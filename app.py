fec07449cb9c270d52432e334e4b58d97700e916
alex-toy <alexei.80@hotmail.fr>
2021-05-16 15:18:07 +0200

    progress database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import sqlite3
+import pandas as pd
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -11,20 +12,22 @@ db = SQLAlchemy(app)
 
 def test():
     query = f"""
-        SELECT * FROM technos
-        WHERE type = '{application_type}';
+        SELECT * FROM sales;
     """
-    conn = sqlite3.connect('developpers.db')
+    conn = sqlite3.connect('data.db')
     df = pd.read_sql(query, conn)
 
-    for index, row in df.iterrows():
-        content = f"{row['name']}"
-        dispatcher.utter_message(text=content)
+    return df
+
+    # for index, row in df.iterrows():
+    #     content = f"{row['name']}"
+    #     dispatcher.utter_message(text=content)
 
 
 
 @app.route('/')
 def index():
+    tasks = test()
     return render_template('index.html', tasks=tasks)
 
 
--- create_db.py
+++ create_db.py
@@ -6,51 +6,229 @@ def create_ville() :
     c = conn.cursor()    
 
     create_villes_query = """
-        CREATE TABLE IF NOT EXISTS technos (
-            id_techno integer,
-            name text,
-            type text
+        CREATE TABLE IF NOT EXISTS villes (
+            id_ville integer,
+            lib_ville text,
+            lib_continent text,
+            lib_pays text,
+            lib_departement text,
+            lib_reg_anc text,
+            lib_reg_nouv text
         )
     """
-
     c.execute(create_villes_query)
+
+
+    myfile = open("data/ville_000.csv", "r")
+    records = myfile.readlines()
+    myfile.close()
+
+    villes = [
+        ( 
+            int(record[:-1].split('|')[0]),
+            record[:-1].split('|')[1],
+            record[:-1].split('|')[2],
+            record[:-1].split('|')[3],
+            record[:-1].split('|')[4],
+            record[:-1].split('|')[5],
+            record[:-1].split('|')[6],
+        )
+        for record in records
+    ]
+
+    insert_villes_qry = """
+        INSERT INTO villes VALUES (?, ?, ?, ?, ?, ?, ?)
+    """
+    c.executemany(insert_villes_qry, villes)
     conn.commit()
     conn.close()
+    print('end create_ville')
 
-    print('end create_tables')
 
 
-def create_tables() :
-    conn = sqlite3.connect('developpers.db')
-    c = conn.cursor()
 
-    conn = sqlite3.connect('developpers.db')
-    c = conn.cursor()
+def create_sales() :
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()    
 
-    create_technos_query = """
-        CREATE TABLE IF NOT EXISTS technos (
-            id_techno integer,
-            name text,
-            type text
+    create_query = """
+        CREATE TABLE IF NOT EXISTS sales (
+            id_ville integer,
+            id_temps integer,
+            id_famille_produit integer,
+            id_magasin integer,
+            ventes_objectif float,
+            vente_reel float,
+            ca_objectif float,
+            ca_reel float,
+            marge_objectif float,
+            marge_reel float
         )
     """
+    c.execute(create_query)
 
-    c.execute(create_developpers_query)
-    c.execute(create_technos_query)
 
+    myfile = open("data/sales_000.csv", "r")
+    records = myfile.readlines()
+    myfile.close()
+
+
+    records_list = [
+        ( 
+            int(record[:-1].split('|')[0]),
+            int(record[:-1].split('|')[1]),
+            int(record[:-1].split('|')[2]),
+            int(record[:-1].split('|')[3]),
+            float(record[:-1].split('|')[4]),
+            float(record[:-1].split('|')[5]) if len(record[:-1].split('|')[5]) > 0 else 0,
+            float(record[:-1].split('|')[6]),
+            float(record[:-1].split('|')[7]) if len(record[:-1].split('|')[5]) > 0 else 0,
+            float(record[:-1].split('|')[8]),
+            float(record[:-1].split('|')[9]) if len(record[:-1].split('|')[5]) > 0 else 0,
+        )
+        for record in records
+    ]
+
+    insert_qry = """
+        INSERT INTO sales VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """
+    c.executemany(insert_qry, records_list)
     conn.commit()
     conn.close()
+    print('end create_sales')
 
-    print('end create_tables')
+
+
+def create_temps() :
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()    
+
+    create_query = """
+        CREATE TABLE IF NOT EXISTS temps (
+            id_temps integer,
+            annee integer,
+            semestre integer,
+            trimestre integer,
+            mois integer,
+            lib_mois text
+        )
+    """
+    c.execute(create_query)
+
+
+    myfile = open("data/temps_000.csv", "r")
+    records = myfile.readlines()
+    myfile.close()
+
+
+    records_list = [
+        ( 
+            int(record[:-1].split('|')[0]),
+            int(record[:-1].split('|')[1]),
+            int(record[:-1].split('|')[2]),
+            int(record[:-1].split('|')[3]),
+            int(record[:-1].split('|')[4]),
+            record[:-1].split('|')[5]
+        )
+        for record in records
+    ]
+
+    insert_qry = """
+        INSERT INTO temps VALUES (?, ?, ?, ?, ?, ?)
+    """
+    c.executemany(insert_qry, records_list)
+    conn.commit()
+    conn.close()
+    print('end create_temps')
+
+
+
+
+
+def create_magasin() :
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()    
+
+    create_query = """
+        CREATE TABLE IF NOT EXISTS magasin (
+            id_magasin integer,
+            lib_magasin text,
+            id_enseigne integer
+        )
+    """
+    c.execute(create_query)
+
+    myfile = open("data/magasin_000.csv", "r")
+    records = myfile.readlines()
+    myfile.close()
+
+    records_list = [
+        ( 
+            int(record[:-1].split('|')[0]),
+            record[:-1].split('|')[1],
+            int(record[:-1].split('|')[2])
+        )
+        for record in records
+    ]
+
+    insert_qry = """
+        INSERT INTO magasin VALUES (?, ?, ?)
+    """
+    c.executemany(insert_qry, records_list)
+    conn.commit()
+    conn.close()
+    print('end create_magasin')
+
+
+
+
+
+def create_famille_produit() :
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()    
+
+    create_query = """
+        CREATE TABLE IF NOT EXISTS famille_produit (
+            id_famille_produit integer,
+            lib_famille_produit text
+        )
+    """
+    c.execute(create_query)
+
+    myfile = open("data/famille_produit_000.csv", "r")
+    records = myfile.readlines()
+    myfile.close()
+
+    records_list = [
+        ( 
+            int(record[:-1].split('|')[0]),
+            record[:-1].split('|')[1]
+        )
+        for record in records
+    ]
+
+    insert_qry = """
+        INSERT INTO famille_produit VALUES (?, ?)
+    """
+    c.executemany(insert_qry, records_list)
+    conn.commit()
+    conn.close()
+    print('end create_famille_produit')
+
+
+
 
 
 def delete_all() :
     
-    conn = sqlite3.connect('developpers.db')
+    conn = sqlite3.connect('data.db')
     c = conn.cursor()
 
-    c.execute("DROP TABLE developpers;")
-    c.execute("DROP TABLE technos;")
+    c.execute("DROP TABLE villes;")
+    c.execute("DROP TABLE sales;")
+    c.execute("DROP TABLE temps;")
+    c.execute("DROP TABLE magasin;")
+    c.execute("DROP TABLE famille_produit;")
 
     conn.commit()
     conn.close()
@@ -61,51 +239,14 @@ def delete_all() :
 
 
 
-def populate_tables() :
-    conn = sqlite3.connect('developpers.db')
-    c = conn.cursor()
+
+
+def show_tables(table_name):
     
-    developpers = [
-        ('julie', 'garandet', 1, 7),
-        ('mathieux', 'bordet', 'react', 10),
-        ('julien', 'ducros', 3, 5),
-        ('valentine', 'penicaud', 2, 5),
-        ('marc', 'sanders', 1, 15)
-    ]
-    insert_developpers_qry = """
-        INSERT INTO developpers VALUES (?, ?, ?, ?)
-    """
-    c.executemany(insert_developpers_qry, developpers)
-
-    technos = [
-        (1, 'node.js', 'web'),
-        (2, 'react native', 'mobile'),
-        (3, 'java', 'web'),
-        (4, 'swift', 'mobile')
-    ]
-    insert_technos_qry = """
-        INSERT INTO technos VALUES (?, ?, ?)
-    """    
-    c.executemany(insert_technos_qry, technos)
-
-    conn.commit()
-    conn.close()
-
-    print('end populate_tables')
-
-
-
-def show_tables():
-    
-    conn = sqlite3.connect('developpers.db')
+    conn = sqlite3.connect('data.db')
     c = conn.cursor()
 
-    c.execute("SELECT * FROM developpers WHERE first_name = 'alessio';" )
-    results = c.fetchall()
-    print(results)
-
-
-    c.execute("SELECT * FROM technos;" )
+    c.execute(f"SELECT * FROM {table_name};" )
     results = c.fetchall()
     print(results)
 
@@ -120,5 +261,17 @@ def show_tables():
 
 if __name__ == "__main__":
 
-    create_ville()
+    #delete_all()
+
+    #create_ville()
+
+    #create_sales()
+
+    #create_temps()
+
+    #create_magasin()
+
+    create_famille_produit()
+
+    show_tables('famille_produit')
 
