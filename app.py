95cbbe296713d27bf78f0f4416cb630768cabd86
alex-toy <alexei.80@hotmail.fr>
2021-05-17 09:31:18 +0200

    progress details

--- app.py
+++ app.py
@@ -102,18 +102,20 @@ def details():
         hifi_ca_reel=di["hifi_ca_reel"],
         hifi_ventes_obj=di["hifi_ventes_obj"],
         hifi_ventes_reel=di["hifi_ventes_reel"],
-        hifi_marge_obj=di["hifi_ventes_obj"],
-        hifi_marge_reel=di["hifi_ventes_reel"],
+        hifi_marge_obj=di["hifi_marge_obj"],
+        hifi_marge_reel=di["hifi_marge_reel"],
 
-        fours_ca_obj=di["fours_ca_obj"]
+        fours_ca_obj=di["fours_ca_obj"],
+        fours_ca_reel=di["fours_ca_reel"],
+        fours_ventes_obj=di["fours_ventes_obj"],
+        fours_ventes_reel=di["fours_ventes_reel"],
+        fours_marge_obj=di["fours_marge_obj"],
+        fours_marge_reel=di["fours_marge_reel"],
+
+        hifi_kpi=di["hifi_kpi"],
+        fours_kpi=di["fours_kpi"],
+        magneto_kpi=di["magneto_kpi"],
     )
-
-
-
-
-
-
-
 
 
 
