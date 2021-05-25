4aad44f7af9c2346c81bfc0c4d4a1a5964e14f5c
alex-toy <alexei.80@hotmail.fr>
2021-05-25 09:11:10 +0200

    progress user model

--- create_user.py
+++ create_user.py
@@ -6,43 +6,42 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 if __name__ == "__main__":
 
+
+    myfile = open("data/staging_utilisateur_000", "r")
+    records = myfile.readlines()
+    myfile.close()
+
+    user_list = [
+        ( 
+            record[:-1].split('|')[0],
+            record[:-1].split('|')[1],
+            record[:-1].split('|')[2],
+            record[:-1].split('|')[3],
+            record[:-1].split('|')[4],
+            int(record[:-1].split('|')[5]) if len(record[:-1].split('|')[5]) > 0 else None,
+            int(record[:-1].split('|')[6]) if len(record[:-1].split('|')[6]) > 0 else None,
+            int(record[:-1].split('|')[7]) if len(record[:-1].split('|')[7]) > 0 else None,
+            int(record[:-1].split('|')[8]) if len(record[:-1].split('|')[8]) > 0 else None,
+        )
+        for record in records
+    ]
+
+    #print(records_list)
+
     
     db.create_all(app = create_app())
 
     app = create_app()
     with app.app_context():
 
-        new_user = User(
-            email='alexei.80@hotmail.fr', 
-            name='alex', 
-            password=generate_password_hash('alex', method='sha256'), 
-            user_type='admin'
-        )
-        db.session.add(new_user)
-        
-        new_user = User(
-            email='splaya@agapei.asso.fr', 
-            name='splaya', 
-            password=generate_password_hash('splaya', method='sha256'), 
-            user_type='dir_gen'
-        )
-        db.session.add(new_user)
-        
-        new_user = User(
-            email='touaaatimahe@gmail.com', 
-            name='touaaatimahe', 
-            password=generate_password_hash('touaaatimahe', method='sha256'), 
-            user_type='dir_reg1'
-        )
-        db.session.add(new_user)
+        for user in user_list :   #('babe', 'rene', 'rene_babe', 'babe', 'rene_babe@darties.com', 1, None, None, 2021)
 
-        new_user = User(
-            email='oussama.garraouii@gmail.com', 
-            name='oussama', 
-            password=generate_password_hash('oussama', method='sha256'), 
-            user_type='dir_reg2'
-        )
-        db.session.add(new_user)
+            new_user = User(  
+                email='alexei.80@hotmail.fr', 
+                name='alex', 
+                password=generate_password_hash('alex', method='sha256'), 
+                user_type='admin'
+            )
+            db.session.add(new_user)
         
-        
-        db.session.commit()
+            db.session.commit()
