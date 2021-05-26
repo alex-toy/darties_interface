9b4b4fd06390595edf13d0eeea63956ec943c61f
alex-toy <alexei.80@hotmail.fr>
2021-05-26 10:33:38 +0200

    progress regional ranks

--- queries_mag.py
+++ queries_mag.py
@@ -44,9 +44,6 @@ def region_containing(id_mag) :
         lib_departement = result[0][0]
     
 
-
-    print(lib_departement)
-
     if lib_departement in dep_reg_1 : return 1
     if lib_departement in dep_reg_2 : return 2
     if lib_departement in dep_reg_3 : return 3
@@ -56,38 +53,6 @@ def region_containing(id_mag) :
     return 0
 
 
-
-
-# def performances_nationales(annee, mois_int):
-#     conn = sqlite3.connect('data.db')
-
-#     query = """
-#         SELECT sum({}) 
-        
-#         FROM sales
-#         JOIN temps ON sales.id_temps = temps.id_temps
-        
-#         WHERE 
-#         temps.annee = {} AND
-#         temps.mois = {};
-#     """
-#     ca_obj = pd.read_sql(query.format('ca_objectif', annee, mois_int), conn).values[0][0]
-#     ca_reel = pd.read_sql(query.format('ca_reel', annee, mois_int), conn).values[0][0]
-#     ventes_objectif = pd.read_sql(query.format('ventes_objectif', annee,mois_int), conn).values[0][0]
-#     vente_reel = pd.read_sql(query.format('vente_reel', annee, mois_int), conn).values[0][0]
-#     marge_objectif = pd.read_sql(query.format('marge_objectif', annee, mois_int), conn).values[0][0]
-#     marge_reel = pd.read_sql(query.format('marge_reel', annee, mois_int), conn).values[0][0]
-
-#     conn.close()
-
-#     return {
-#         "ca_obj" : ca_obj,
-#         "ca_reel" : ca_reel,
-#         "ventes_objectif" : ventes_objectif,
-#         "vente_reel" : vente_reel,
-#         "marge_objectif" : marge_objectif,
-#         "marge_reel" : marge_reel
-#     }
 
 
 
@@ -178,3 +143,120 @@ def mag_name(id_mag) :
 
 
 
+def nat_rank_mag(id_mag, classifier, annee, mois) :
+    conn = sqlite3.connect('data.db')
+
+    query = """
+        SELECT 
+            id_mag, lib_ville, kpi_rank
+        
+        FROM (
+            SELECT 
+                villes.lib_ville AS lib_ville,
+                magasin.id_magasin AS id_mag,
+                sum(sales.{0}) AS kpi_sales,
+                RANK () OVER ( ORDER BY sum(sales.{0}) DESC) AS kpi_rank
+                
+
+            FROM
+                sales
+                JOIN temps ON sales.id_temps = temps.id_temps
+                JOIN villes ON sales.id_ville = villes.id_ville
+                JOIN magasin ON magasin.lib_magasin = villes.lib_ville
+
+            WHERE 
+                temps.annee = {1} AND
+                temps.mois = {2}
+            
+            GROUP BY villes.lib_ville
+            ) innner
+
+        WHERE
+            innner.id_mag = {3}
+        ;
+    """
+
+    formated_query = query.format(classifier, annee, mois, id_mag)
+
+    result = pd.read_sql(formated_query, conn).values
+    conn.close()
+
+    if len(result) > 0:
+        return result[0][2]
+
+    return result
+
+
+
+
+
+def nat_rank_mag_item(id_mag, classifier, annee, mois, id_famille_produit) :
+    conn = sqlite3.connect('data.db')
+
+    query = """
+    SELECT v, RANK () OVER (  ORDER BY {}) rank_no 
+    
+    FROM
+        sales.rank_demo;
+    """
+
+    query = """
+        SELECT 
+            RANK () OVER (  ORDER BY sum({})) rank_no
+
+            sum(ca_objectif), sum({}), 
+            sum(ventes_objectif), sum(vente_reel), 
+            sum(marge_objectif), sum(marge_reel)
+        
+        FROM sales
+        JOIN temps ON sales.id_temps = temps.id_temps
+        JOIN villes ON sales.id_ville = villes.id_ville
+        JOIN magasin ON magasin.lib_magasin = villes.lib_ville
+        JOIN famille_produit ON sales.id_famille_produit = famille_produit.id_famille_produit
+        
+        WHERE 
+            temps.annee = {} AND
+            temps.mois = {} AND
+            magasin.id_magasin = {} AND
+            famille_produit.id_famille_produit = {};
+    """
+
+    query = """
+        SELECT 
+            id_mag, lib_ville, kpi_rank
+        
+        FROM (
+            SELECT 
+                villes.lib_ville AS lib_ville,
+                magasin.id_magasin AS id_mag,
+                sum(sales.{0}) AS kpi_sales,
+                RANK () OVER ( ORDER BY sum(sales.{0}) DESC) AS kpi_rank
+                
+
+            FROM
+                sales
+                JOIN temps ON sales.id_temps = temps.id_temps
+                JOIN villes ON sales.id_ville = villes.id_ville
+                JOIN magasin ON magasin.lib_magasin = villes.lib_ville
+                JOIN famille_produit ON sales.id_famille_produit = famille_produit.id_famille_produit
+
+            WHERE 
+                temps.annee = {1} AND
+                temps.mois = {2} AND
+                famille_produit.id_famille_produit = {4}
+            
+            GROUP BY villes.lib_ville
+            ) innner
+
+        WHERE
+            innner.id_mag = {3}
+        ;
+    """
+
+    result = pd.read_sql(query.format(classifier, annee, mois, id_mag, id_famille_produit), conn).values
+    conn.close()
+
+    if len(result) > 0:
+        return result[0][2]
+
+    return result
