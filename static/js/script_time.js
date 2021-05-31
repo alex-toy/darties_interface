a9bc3b692125584ad7519f87ab3fb89702202101
alex-toy <alexei.80@hotmail.fr>
2021-05-31 15:06:04 +0200

    progress interface

--- static/js/script_time.js
+++ static/js/script_time.js
@@ -1,5 +1,48 @@
-function update() {
-    console.log('test')
+function get_load_date(year, monthIndex) {
+    const load_date = new Date(year, monthIndex);
+    return load_date
+}
+
+
+function get_delta() {
+    var year = document.getElementById("year-select").value;
+    const today = new Date()
+    const current_year = today.getFullYear();
+
+    delta = current_year - year
+
+    return delta
+}
+
+
+
+function get_html_months() {
+    delta = get_delta();
+    const current_month = today.getMonth() + 1;
+
+    var html = '<select style="width:150px;height:30px" name="mois" id="mois-select">'
+    const month_list = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
+
+    var option_prev = '<option value="'
+    var option_next = '</option>'
+
+    var i;
+    var init = 0;
+    if(delta < 2){
+        html += '<option value="1|janvier">--Choix du mois--</option>'
+    } else {
+        var init = current_month + 1;
+        html += option_prev + init + '|' + month_list[current_month] + '">' +  month_list[current_month] + option_next;
+    }
+    
+    for (i = init; i < month_list.length; i++) { 
+        month_int = i+1
+        html += option_prev + month_int + '|' + month_list[i] + '">' +  month_list[i] + option_next;
+    }
+    
+    html += '</select>'
+
+    document.getElementById("month_select").innerHTML = html;
 }
 
 
