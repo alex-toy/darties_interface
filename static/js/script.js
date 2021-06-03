e84a03c6cbdf312be79af215831fcff0cecd0281
alex-toy <alexei.80@hotmail.fr>
2021-06-03 16:13:50 +0200

    progress cumul

--- static/js/script.js
+++ static/js/script.js
@@ -30,6 +30,7 @@ function myFunction_haute_savoie(e) {
     document.getElementById('myPopup-savoie').classList.remove('show');
     document.getElementById('myPopup-ain').classList.remove('show');
     document.getElementById('myPopup-isere').classList.remove('show');
+    document.getElementById("myPopup_aide").classList.remove('show');
 
     var popup = document.getElementById("myPopup-haute-savoie");
     popup.classList.toggle("show");
@@ -41,6 +42,7 @@ function myFunction_savoie(e) {
     document.getElementById('myPopup-ain').classList.remove('show');
     document.getElementById('myPopup-haute-savoie').classList.remove('show');
     document.getElementById('myPopup-isere').classList.remove('show');
+    document.getElementById("myPopup_aide").classList.remove('show');
 
     var popup = document.getElementById("myPopup-savoie");
     popup.classList.toggle("show");
@@ -52,6 +54,7 @@ function myFunction_ain(e) {
     document.getElementById('myPopup-haute-savoie').classList.remove('show');
     document.getElementById('myPopup-savoie').classList.remove('show');
     document.getElementById('myPopup-isere').classList.remove('show');
+    document.getElementById("myPopup_aide").classList.remove('show');
 
     var popup = document.getElementById("myPopup-ain");
     popup.classList.toggle("show");
@@ -63,7 +66,7 @@ function myFunction_isere(e) {
     document.getElementById('myPopup-haute-savoie').classList.remove('show');
     document.getElementById('myPopup-savoie').classList.remove('show');
     document.getElementById('myPopup-ain').classList.remove('show');
-    document.getElementById("myPopup_aide").innerHTML = "";
+    document.getElementById("myPopup_aide").classList.remove('show');
 
     var popup = document.getElementById("myPopup-isere");
     popup.classList.toggle("show");
