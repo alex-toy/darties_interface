f46438552d9e4d5573cd31283e86298639d1db8b
alex-toy <alexei.80@hotmail.fr>
2021-06-03 10:18:25 +0200

    progress interface

--- static/js/script.js
+++ static/js/script.js
@@ -42,9 +42,18 @@ function myFunction_ain(e) {
 function myFunction_isere(e) {
     document.getElementById('myPopup-haute-savoie').classList.remove('show');
     document.getElementById('myPopup-savoie').classList.remove('show');
-    document.getElementById('myPopup-haute-savoie').classList.remove('show');
+    document.getElementById('myPopup-ain').classList.remove('show');
 
     var popup = document.getElementById("myPopup-isere");
+    popup.classList.toggle("show");
+    e.stopPropagation();
+}
+
+
+
+function myFunction_aide(e) {
+
+    var popup = document.getElementById("myPopup_aide");
     popup.classList.toggle("show");
     e.stopPropagation();
 }
